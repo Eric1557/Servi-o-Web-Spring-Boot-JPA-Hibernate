12426153f12e4d34234e34be5b5bf13925af6c15
Eric1557 <ericbbarbosa2019@gmail.com>
2025-11-26 20:10:07 -0300

    User entity and resource

---
+++ src/main/java/com/educandoweb/courseSpring/resources/UserResources.java
@@ -0,0 +1,26 @@
+package com.educandoweb.courseSpring.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.educandoweb.courseSpring.entities.User;
+
+@RestController
+@RequestMapping(value= "/users")
+public class UserResources {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+	   User u = new User(1,"Eric","eric@gmail.com","12345","12345");
+	   return ResponseEntity.ok().body(u);
+		
+	}
+	
+	
+	
+	
+	
+
+}
