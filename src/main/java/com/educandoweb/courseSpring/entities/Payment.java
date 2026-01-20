7489d3d96d00a32d25b33216f67183a38593b78f
Eric1557 <ericbbarbosa2019@gmail.com>
2026-01-20 16:07:13 -0300

    Subtotal & Total methods

--- src/main/java/com/educandoweb/courseSpring/entities/Payment.java
+++ src/main/java/com/educandoweb/courseSpring/entities/Payment.java
@@ -3,10 +3,13 @@ package com.educandoweb.courseSpring.entities;
 import java.time.Instant;
 import java.util.Objects;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.MapsId;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
@@ -18,7 +21,9 @@ public class Payment {
 	private Integer id;
 	private Instant moment;
 	
+	@JsonIgnore
 	@OneToOne
+	@MapsId
 	private Order order;
 	
     public Payment(){
